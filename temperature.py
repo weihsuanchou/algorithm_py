748fed9f229633798f1121044a5c201546da4c2f
RiverChou <whc@novataris.com>
2019-04-29 17:08:56 -0500

    addpractice

---
+++ temperature.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, list: List[int]) -> List[int]: 
+      # try 1
+      result = [0] * len(list)
+      stack = []
+      for i in range(len(list)): 
+    
+        while len(stack) > 0 and list[stack[-1]] < list[i]: 
+          # it is an index 
+          cur = stack.pop() # = stack[-1]
+          result[cur] = i-cur # i will always greater than cur because cur is from previous i
+            
+        #only store the number has not found another greater than it
+        stack.append(i) 
+
+      return result
